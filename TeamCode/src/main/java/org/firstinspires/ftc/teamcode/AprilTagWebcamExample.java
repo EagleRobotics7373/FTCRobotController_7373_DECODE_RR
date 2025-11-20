0cde0f79d6cb1b6a3904c4065034022b629d94e3
tscrivner26 <147932620+tscrivner26@users.noreply.github.com>
2025-11-20 17:01:52 -0500

    Update AprilTagWebcamExample.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagWebcamExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagWebcamExample.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.teamcode.mechanisms.AprilTagWebcam;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
+
 @Autonomous
 public class AprilTagWebcamExample extends OpMode {
     AprilTagWebcam aprilTagWebcam = new AprilTagWebcam();
@@ -20,10 +21,32 @@ public class AprilTagWebcamExample extends OpMode {
         //update the vision portal
         aprilTagWebcam.update();
         AprilTagDetection id21 = aprilTagWebcam.getTagBySpecificId(21);
+        AprilTagDetection id22 = aprilTagWebcam.getTagBySpecificId(22);
+        AprilTagDetection id23 = aprilTagWebcam.getTagBySpecificId(23);
         if (id21 != null) {
             telemetry.addData("id21", id21.toString());
+            telemetry.addData("Tag 21 X", id21.ftcPose.x);
+            telemetry.addData("Tag 21 Y", id21.ftcPose.y);
+            telemetry.addData("Tag 21 Z", id21.ftcPose.z);
         } else {
             telemetry.addData("id21", "Not detected");
         }
+        if (id22 != null) {
+            telemetry.addData("id22", id22.toString());
+            telemetry.addData("Tag 22 X", id22.ftcPose.x);
+            telemetry.addData("Tag 22 Y", id22.ftcPose.y);
+            telemetry.addData("Tag 22 Z", id22.ftcPose.z);
+        } else {
+            telemetry.addData("id22", "Not detected");
+        }
+        if (id21 != null) {
+            telemetry.addData("id23", id23.toString());
+            telemetry.addData("Tag 23 X", id23.ftcPose.x);
+            telemetry.addData("Tag 23 Y", id23.ftcPose.y);
+            telemetry.addData("Tag 23 Z", id23.ftcPose.z);
+        } else {
+            telemetry.addData("id23", "Not detected");
+        }
+
     }
 }
